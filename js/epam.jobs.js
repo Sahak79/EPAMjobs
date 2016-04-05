32dc32f792a7be0b441998a448f110ee12f7c91a
Sahak_Babayan <sahak79@hotmail.com>
2016-04-05 19:37:01 +0400

    initial commit

--- js/epam.jobs.js
+++ js/epam.jobs.js
@@ -1,60 +1,68 @@
-var EPAMstore = (function($) {
+var EPAMjobs = (function($) {
 
     function init() {
         $('#menu_more').on('click', function() {
-            if($(this).hasClass("opened")) {
-                $(this).removeClass("opened");
-                $(this).addClass("closed");
-                $('.eHeaderBottom').slideUp();
-            }else if($(this).hasClass("closed")) {
-                $(this).removeClass("closed");
-                $(this).addClass("opened");
-                $('.eHeaderBottom').slideDown();
+            var that = this;
+            var eHeaderBottom = $('.eHeaderBottom');
+            if($(that).hasClass("opened")) {
+                $(that).removeClass("opened");
+                $(that).addClass("closed");
+                eHeaderBottom.slideUp();
+            }else if($(that).hasClass("closed")) {
+                $(that).removeClass("closed");
+                $(that).addClass("opened");
+                eHeaderBottom.slideDown();
             }else {
-                $(this).addClass("opened");
-                $('.eHeaderBottom').slideDown();
+                $(that).addClass("opened");
+                eHeaderBottom.slideDown();
             }
         });
 
-        ensureShoesItemHeight();
+        // attach click function onclick for tab menu items
+        var eTabMenu = document.getElementById('eTabMenuItems');
+        var eTabMenuItems = eTabMenu.getElementsByTagName("a");
+        for (var i = 0; i < eTabMenuItems.length; i++) {
+            var tabNumber = i+1;
+            $(eTabMenuItems[i]).attr("onclick", "EPAMjobs.shopTab(this, "+tabNumber+")");
+        }
+
+        ensureAdvertisingItemHeight();
 
         $(window).resize(function() {
             var menu_more = $('#menu_more');
+            var eHeaderBottom = $('.eHeaderBottom');
             if($(window).width() > 720) {
-                $('.eHeaderBottom').slideDown(10);
+                eHeaderBottom.slideDown(10);
                 menu_more.removeClass("closed");
                 menu_more.removeClass("opened");
+
             }else {
-                $('.eHeaderBottom').slideUp(10);
+                eHeaderBottom.slideUp(10);
                 menu_more.removeClass("opened");
                 menu_more.addClass("closed");
+
             }
-
             // ensure main container items height
-            ensureShoesItemHeight();
-
+            ensureAdvertisingItemHeight();
         });
+
     }
 
-    function ensureShoesItemHeight() {
+    function ensureAdvertisingItemHeight() {
         var mainContainerItem = $('.eCommerceMain .eMainCenter .eShoesContainer1>div');
-        var mainContainerItemWidth = $('.eCommerceMain .eMainCenter .eShoesContainer1>div').width();
+        var mainContainerItemWidth = mainContainerItem.width();
         mainContainerItem.css('height', mainContainerItemWidth+'px');
     }
 
     function shopTab(element, selector) {
-        var active = $('.eTabMenuItems .active');
-        var activeLabel = $('.ePlaceholderContainer .active');
-        active.removeClass('active');
-        activeLabel.removeClass('active');
+        $('.eTabTop .active').removeClass('active');
+        $('.ePlaceholderContainer .active').removeClass('active');
         $(element).addClass('active');
         $('#tabPlaceholder'+selector).addClass('active');
-
         $('.eCurrentTab').hide();
         var appropriateTab = $('#eCurrentTab'+selector);
         appropriateTab.show();
     }
-
 
     return{
         init: init,
@@ -63,5 +71,5 @@ var EPAMstore = (function($) {
 })(jQuery);
 
 window.onload = function() {
-    EPAMstore.init();
-}
+    EPAMjobs.init();
+};
